a1006aeb94b03aa1e1367e2657b51a0122e8825d
patillacode <patillacode@gmail.com>
2015-12-07 23:30:07 +0000

    DAY 04 - Success

---
+++ days/04/part1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import hashlib
+
+
+key = 'ckczppom'
+keep_going = True
+number = -1
+while keep_going:
+    number += 1
+    md5_hash = hashlib.md5('{0}{1}'.format(key, number)).hexdigest()
+    if md5_hash.startswith('00000'):
+        keep_going = False
+
+print 'Number: {0}'.format(number)
